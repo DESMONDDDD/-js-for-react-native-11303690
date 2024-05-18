8c815bff3af5eb9b1621392a15b761c83f0e0108
DESMONDDDD <dadarkwa004@st.ug.edu.gh>
2024-05-18 17:43:32 +0100

    Update arrayManipulation.js

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,23 +1,9 @@
 
 // arrayManipulation.js
-
-/**
- * Function to process an array of numbers.
- * Squares even numbers and triples odd numbers.
- * @param {number[]} arr - Array of numbers.
- * @returns {number[]} - Processed array.
- */
 function processArray(arr) {
   return arr.map(num => (num % 2 === 0 ? num * num : num * 3));
 }
 
-/**
-* Function to format an array of strings based on the corresponding array of numbers.
-* Capitalizes strings if the corresponding number is even, converts to lowercase if odd.
-* @param {string[]} stringArr - Array of strings.
-* @param {number[]} numArr - Array of numbers processed by processArray.
-* @returns {string[]} - Formatted array of strings.
-*/
 function formatArrayStrings(stringArr, numArr) {
   return stringArr.map((str, index) => (numArr[index] % 2 === 0 ? str.toUpperCase() : str.toLowerCase()));
 }
